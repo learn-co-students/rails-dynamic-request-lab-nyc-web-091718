e223286e6bc7de58870cfc00646306f5dee772b0
mx@E <mglxp5@gmail.com>
2018-10-11 17:29:43 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,10 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show
+    @student = Student.find{|s| s.id == params[:id].to_i}
+    #binding.pry
+  end
+
 end
